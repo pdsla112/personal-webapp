6c514cdb3bf4b2fb7a871e48f25f3fd7fe9f5beb
u6943702 <u6943702@anu.edu.au>
2021-03-07 10:03:02 +1100

    Add header.css and link to index.html, and add more html code to Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,8 +3,11 @@ import React from "react";
 class Header extends React.Component {
     render() {
         return (
-            <div>
-
+            <div id="header-box">
+                <h1>Phillip La</h1>
+                <p>Why I Am Prepared for Your Opportunity</p>
+                <p>Personal Projects</p>
+                <p>Extra Hobbies</p>
             </div>
         );
     }
