eae9e9c905c944f098a4499128d9fe0ab5e5d20c
u6943702 <u6943702@anu.edu.au>
2021-03-08 16:02:52 +1100

    Crop wave png file and debug bottom decoration to display as wanted

--- client/src/FixedBottomDecoration.js
+++ client/src/FixedBottomDecoration.js
@@ -1,19 +1,13 @@
 import React from "react";
-import waves from "./images/sea.png";
+import waves from "./images/edited-sea.png";
 import tree from "./images/pine-2.png";
 
 class FixedBottomDecoration extends React.Component {
     render() {
         return (
             <div className="fixed-bottom-decoration-box">
-                {/* <img src={waves} className="wave-img"/>
-                <img src={tree} className="tree-img"/> */}
-                <div className="wave-box">
-                    <img src={waves} className="wave-img"/>
-                </div>
-                <div className="tree-box">
-                    <img src={tree} className="tree-img"/>
-                </div>
+                <img src={waves} className="wave-img"/>
+                <img src={tree} className="tree-img"/>
             </div>
         );
     }
