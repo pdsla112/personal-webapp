59d3ec42c09f5b6d3ccd6ffe0718d817fb96d36e
u6943702 <u6943702@anu.edu.au>
2021-03-08 17:42:27 +1100

    Add code for second home box

--- client/src/HomeSecondBox.js
+++ client/src/HomeSecondBox.js
@@ -3,8 +3,20 @@ import React from "react";
 class HomeSecondBox extends React.Component {
     render() {
         return (
-            <div>
-
+            <div id="home-second-box">
+                <h2 className="home-second-box-heading">Why I Am Ready to Work for You</h2>
+                <p className="home-second-box-txt"> 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque at eros vel diam elementum imperdiet.
+                    Aliquam faucibus sodales ipsum, ut scelerisque purus vulputate et.
+                    Donec tempor sed velit in dictum.
+                    Praesent mollis interdum libero, vel ultrices ante rutrum eget.
+                    Curabitur id dictum elit.
+                    Praesent libero velit, dignissim pulvinar ex id, viverra faucibus odio.
+                    Nullam aliquam augue ac nibh fermentum bibendum.
+                    Vestibulum consequat orci ut metus convallis, quis hendrerit ligula blandit.
+                    Aenean posuere volutpat ultrices. Quisque in dolor enim.
+                </p>
             </div>
         );
     }
