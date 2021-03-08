aab039060bd5365a47a19faac0f978d6d9beb3e1
u6943702 <u6943702@anu.edu.au>
2021-03-08 18:16:05 +1100

    Add third and fourth box and most of its contents

--- client/src/HomeFourthBox.js
+++ client/src/HomeFourthBox.js
@@ -3,8 +3,15 @@ import React from "react";
 class HomeFourthBox extends React.Component {
     render() {
         return (
-            <div>
-
+            <div id="home-fourth-box">
+                <h2>My Other Hobbies</h2>
+                <p>
+                    I love building gaming computers!
+                    The idea of thinking through the compatibility of different components
+                    of my PC, and imagining its final form, is exciting and nerdy.
+                    Also, you can play the latest games on it without worrying too much
+                    about storage/graphic issues.
+                </p>
             </div>
         );
     }
--- client/src/HomeThirdBox.js
+++ client/src/HomeThirdBox.js
@@ -3,8 +3,23 @@ import React from "react";
 class HomeThirdBox extends React.Component {
     render() {
         return (
-            <div>
-
+            <div id="home-third-box">
+                <h2>My Projects</h2>
+                <h3>Web Applications That I Developed</h3>
+                <ul>
+                    <li>This One!</li>
+                        <ul>
+                            <li>Github Repository for this Web Application</li>
+                        </ul>
+                    <li>Reach Magazine App</li>
+                        <ul>
+                            <li>Click here to visit the website...</li>
+                            <li>Github Respotiory for this Web Application</li>
+                            <li>View My Story Behind This App</li>
+                        </ul>
+                </ul>
+                <h3>A Java Github Repository That I Have for Learning Algorithms and Data Types</h3>
+                <img></img>
             </div>
         );
     }
