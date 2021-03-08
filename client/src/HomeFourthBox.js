f03895d17176a191e4f36504248fac592552d700
u6943702 <u6943702@anu.edu.au>
2021-03-08 18:33:05 +1100

    Add more content into the fourth home box

--- client/src/HomeFourthBox.js
+++ client/src/HomeFourthBox.js
@@ -5,12 +5,29 @@ class HomeFourthBox extends React.Component {
         return (
             <div id="home-fourth-box">
                 <h2>My Other Hobbies</h2>
+                <h3>Building Gaming Computers</h3>
                 <p>
                     I love building gaming computers!
                     The idea of thinking through the compatibility of different components
                     of my PC, and imagining its final form, is exciting and nerdy.
                     Also, you can play the latest games on it without worrying too much
                     about storage/graphic issues.
+                </p>
+                <h3>Learning About Human History</h3>
+                <p>
+                    I love watching documentaries and reading books about how
+                    archaeology is being used to uncover secrets about the ancient
+                    past. Learning more about older civilisations allows me to
+                    time travel to that place, and immerse myself into their
+                    culture and vibe. This is probably why I enjoy playing massive open world
+                    games that are set in the past, like <em>Kingdom Come Deliverance</em>,
+                    <em>Assassin's Creed Origins</em> and <em>Assassin's Creed Odyssey</em>.
+                </p>
+                <h3>Eating and Learning About Food</h3>
+                <p>
+                    I know that this might sound kind of obvious. I mean who doesn't like food?
+                    But, I <em>REALLY</em> like food. At the end of a hard day, there is almost 
+                    nothing quite like eating good food, and feeling its satisfying flavours and textures.
                 </p>
             </div>
         );
