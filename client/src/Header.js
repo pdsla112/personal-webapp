6b4e8d9d9001e80f866a6aeb6497535cb71ddb03
u6943702 <u6943702@anu.edu.au>
2021-03-09 18:24:29 +1100

    Position png icons in header box

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
+import githubLogo from "./images/github-logo.png";
+import linkedinLogo from "./images/linkedin-logo.png";
+import mediumLogo from "./images/medium-logo.png";
 
 class Header extends React.Component {
     render() {
         return (
             <div id="header-box">
+                <a href="https://github.com/"><img src={githubLogo} className="header-box-github-logo"/></a>
+                <a href="https://au.linkedin.com/"><img src={linkedinLogo} className="header-box-linkedin-logo"/></a>
+                <a href="https://medium.com/"><img src={mediumLogo} className="header-box-medium-logo"/></a>
                 <h1 className="header-box-title">PHILLIP LA</h1>
                 <p className="header-box-navlink-1">Why I Am Prepared for Your Opportunity</p>
                 <p className="header-box-navlink-2">Personal Projects</p>
