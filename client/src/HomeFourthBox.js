74491bcbb0c7e5d5252ba0540f467a27a02077fc
u6943702 <u6943702@anu.edu.au>
2021-03-16 05:24:04 +1100

    Add flexbox to fourth box in homepage

--- client/src/HomeFourthBox.js
+++ client/src/HomeFourthBox.js
@@ -6,15 +6,19 @@ class HomeFourthBox extends React.Component {
             <div id="home-fourth-box">
                 <h2 className="home-fourth-box-heading">My Other Hobbies</h2>
                 <h3 className="home-fourth-box-subheading">Building Gaming Computers</h3>
-                <p className="home-fourth-box-txt">
+                <div className="home-fourth-box-content-box">
+                    <p className="home-fourth-box-txt">
                     I love building gaming computers!
                     The idea of thinking through the compatibility of different components
                     of my PC, and imagining its final form, is exciting and nerdy.
                     Also, you can play the latest games on it without worrying too much
                     about storage/graphic issues.
-                </p>
+                    </p>
+                </div>
+                
                 <h3 className="home-fourth-box-subheading">Learning About Human History</h3>
-                <p className="home-fourth-box-txt">
+                <div className="home-fourth-box-content-box">
+                    <p className="home-fourth-box-txt">
                     I love watching documentaries and reading books about how
                     archaeology is being used to uncover secrets about the ancient
                     past. Learning more about older civilisations allows me to
@@ -22,13 +26,17 @@ class HomeFourthBox extends React.Component {
                     culture and vibe. This is probably why I enjoy playing massive open world
                     games that are set in the past, like <em>Kingdom Come Deliverance</em>,
                     <em>Assassin's Creed Origins</em> and <em>Assassin's Creed Odyssey</em>.
-                </p>
+                    </p>
+                </div>
+                
                 <h3 className="home-fourth-box-subheading">Eating and Learning About Food</h3>
-                <p className="home-fourth-box-txt">
+                <div className="home-fourth-box-content-box">
+                    <p className="home-fourth-box-txt">
                     I know that this might sound kind of obvious. I mean who doesn't like food?
                     But, I <em>REALLY</em> like food. At the end of a hard day, there is almost 
                     nothing quite like eating good food, and feeling its satisfying flavours and textures.
-                </p>
+                    </p>
+                </div>
             </div>
         );
     }
